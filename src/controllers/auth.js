9f41b6b74ef8867b77e05a81ece6fbed39d2ce9a
abdipriyangga <mabdipme@gmail.com>
2021-08-22 08:56:21 +0700

    add validation, user profile, middleware checkToken

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,23 +1,30 @@
 const authModel = require("../models/auth");
 const {response: formResponse} = require("../helpers/formResponse");
 const argon2 = require("argon2");
+const jwt = require("jsonwebtoken");
+const {APP_SECRET_KEY} = process.env;
+
 
 exports.register = async(req, res) => {
+	const { name, email, password } = req.body;
 	try {
-		const { name, email, password } = req.body;
-		const hashPassword = await argon2.hash(password);
-		const auth = await authModel.create({name: name, email: email, password: hashPassword});
-		return formResponse(res, 200, "register success!", auth);
+		const checkEmail = await authModel.findOne({ where: { email: email } });
+		if(checkEmail !== null) {
+			return formResponse(res, 401, "Email alredy exists!", null);
+		} else {
+			const hashPassword = await argon2.hash(password);
+			const auth = await authModel.create({ name: name, email: email, password: hashPassword });
+			return formResponse(res, 200, "register success!", auth);
+		}
 	} catch (error) {
 		return formResponse(res, 400, "internal failure!", error);
 	}
-
 };
 
 exports.login = async(req, res) => {
 	const {email, password} = req.body;
 	const auth = await authModel.findOne({where: {email: email}});
-	console.log(`password dari db ${auth.password} ${password}`);
+	// console.log(`password dari db ${auth.password} ${password}`);
 	if (auth === null) {
 		return formResponse(res, 404, "user not found!");
 	} else {
@@ -25,7 +32,8 @@ exports.login = async(req, res) => {
 			const checkPass = await argon2.verify(auth.password, password);
 			// console.log("kondisi dua: ", checkPass);
 			if (checkPass) {
-				return formResponse(res, 200, "login success!", { results: auth });
+				const token = jwt.sign({id:auth.id, email: auth.email}, APP_SECRET_KEY, {expiresIn: "2h"});
+				return formResponse(res, 200, "login success!", token);
 			} else {
 				return formResponse(res, 400, "password does'n match!");
 			}
---
+++ src/middleware/checkToken.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const {APP_SECRET_KEY} = process.env;
+const {response:formResponse} = require("../helpers/formResponse");
+
+const auth = (req, res, next) => {
+	console.log(req.headers);
+	if(req.headers.authorization) {
+		if(req.headers.authorization.startsWith("Bearer")) {
+			try {
+				const token = req.headers.authorization.slice(7);
+				const user = jwt.verify(token, APP_SECRET_KEY);
+				req.authUser = user;
+				next();
+			} catch (error) {
+				return formResponse(res, 401, "Session expired, please login!");
+			}
+		}
+	} else {
+		return formResponse(res, 401, "Token needed!");
+	}
+};
+
+module.exports = auth;
